164df41137afdc76961c72b1787d06264ec3bd42
arupchowdhury <38394067+arupchowdhury@users.noreply.github.com>
2018-05-27 23:08:09 +0600

    custom view Account head list

--- app/src/main/java/com/example/arup/personalaccount/CustomListView/AccountHeadListAdapter.java
+++ app/src/main/java/com/example/arup/personalaccount/CustomListView/AccountHeadListAdapter.java
@@ -2,9 +2,14 @@ package com.example.arup.personalaccount.CustomListView;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.TextView;
 
 import com.example.arup.personalaccount.Model.IncomeExpenseHead;
 import com.example.arup.personalaccount.R;
@@ -20,9 +25,10 @@ public class AccountHeadListAdapter extends ArrayAdapter<IncomeExpenseHead> impl
     ArrayList<IncomeExpenseHead>searchIncomeExpense;
     IncomeExpenseHead incomeExpenseHead;
 
-    public AccountHeadListAdapter(@NonNull Context context, int resource, @NonNull List<IncomeExpenseHead> objects) {
-        super(context, R.layout.accountheadlistview, objects);
-        //comment
+    public AccountHeadListAdapter(@NonNull Context context, int resource, @NonNull ArrayList<IncomeExpenseHead> incomeExpenseHeadslist) {
+        super(context, R.layout.accountheadlistview, incomeExpenseHeadslist);
+        this.context=context;
+        this.incomeExpenseHeadslist=incomeExpenseHeadslist;
     }
 
 
@@ -31,5 +37,40 @@ public class AccountHeadListAdapter extends ArrayAdapter<IncomeExpenseHead> impl
         return null;
     }
 
+    private int lastPosition = 0;
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Get the data item for this position
+        incomeExpenseHead = getItem(position);
+        ViewHolder viewHolder;
+        final View result;
+        if(convertView==null){
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.accountheadlistview, parent, false);
+
+            viewHolder.accheadId = (TextView)convertView.findViewById(R.id.tvHeadId);
+            viewHolder.accHeadName=(TextView)convertView.findViewById(R.id.tvAccountHeadName);
+            result = convertView;
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result=convertView;
+        }
+
+        lastPosition = position;
+
+        viewHolder.accheadId.setText(incomeExpenseHead.getHeadId());
+        viewHolder.accHeadName.setText(incomeExpenseHead.getHeadName());
+
+        return convertView;
+    }
+
+    public static class ViewHolder{
+        TextView accheadId;
+        TextView accHeadName;
+    }
 }
